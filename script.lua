d12248b6a442555f4c1f3b5183e34c6cec866d98
afuatesthub <hoclevanhoc962@gmail.com>
2026-01-12 20:36:36 +0700

    Add files via upload

--- script.lua
+++ script.lua
@@ -21,7 +21,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
   })
 
        MinimizeButton({
-       Image = "http://www.roblox.com/asset/?id=134938419645817",
+       Image = "http://www.roblox.com/asset/?id=83190276951914",
        Size = {40, 40},
        Color = Color3.fromRGB(10, 10, 10),
        Corner = true,
